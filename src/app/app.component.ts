64ce7f6982c0d7f3dc6c8b581c81c229dc5ea2ef
Omar Olmos <oolmos@embarkgeneral.com>
2019-09-25 09:26:35 -0700

    Use ngmodel, interpolation, ng template, ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-first-app';
+  username = '';
+
+
+ngOnInit() {
+
+}
+
+onText(event: Event) {
+this.username = 'Hi';
+}
+
+onClick() {
+this.username = '';
+}
+
+
 }
